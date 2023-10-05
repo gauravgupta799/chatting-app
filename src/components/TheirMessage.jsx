c72599440c9545bdf18cf164a39de0653b7fd3c7
gauravgupta <gaurav.mobilefirst@gmail.com>
2023-10-05 19:02:14 +0530

    msg-bg-color-changed

--- src/components/TheirMessage.jsx
+++ src/components/TheirMessage.jsx
@@ -27,8 +27,10 @@ const TheirMessage = ({message, lastMessage}) => {
         <div className='message' 
           style={{
             float:'left',
-            color:"white",
-            backgroundColor:"#CABCDC",
+            color:"#fff",
+            backgroundColor:"#3B2A50",
+            padding:"0.4em",
+            borderRadius:"4px",
             marginRight:isFirstMessageByUser ? "4px" :"48px"
           }}
         >
